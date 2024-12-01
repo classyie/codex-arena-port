e150ce4642f4936fd1955a67055dfb42a9c43f31
Harshit <harshit2001pandey@gmail.com>
2024-12-01 13:50:00 +0530

    Updated Pricing Plans and Submit Form

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   return (
     <>
       <nav
-        className="bg-white border-gray-200 sticky top-0 z-50"
+        className="sticky top-0 z-50 bg-white border-gray-200"
         style={{ backgroundColor: "#F7F9F9" }}
       >
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
@@ -26,12 +26,12 @@ function Navbar() {
             href="/"
           >
             <img src={logo} className="h-8 rounded-lg" alt="Network Error" />
-            <span className="self-center text-2xl whitespace-nowrap font-arkitech dark:text-black font-extrabold">
+            <span className="self-center text-2xl font-extrabold text-black whitespace-nowrap font-arkitech">
               Codex Arena
             </span>
           </a>
           <button
-            className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400"
+            className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-white bg-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             aria-controls="navbar-default"
             style={{ backgroundColor: "#1B1A1D" }}
             aria-expanded={isMenuOpen}
@@ -60,18 +60,16 @@ function Navbar() {
           <div
             className={`${
               isMenuOpen ? "block" : "hidden"
-            } absolute top-0 left-0 w-full mt-16 h-full bg-black bg-opacity-50 transition-all ease-in-out duration-300 z-50 md:hidden ${
-              isMenuOpen ? "translate-x-0" : "-translate-x-full"
-            }`}
+            } absolute top-0 left-0 w-full mt-16 bg-opacity-50 transition-all ease-in-out duration-300 z-50 md:hidden`}
             id="navbar-default"
           >
-            <ul className="flex flex-col p-4 mt-4 font-medium ease-in-out border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-[#F7F9F9] bg-[#F7F9F9]">
+            <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 text-black bg-[#F7F9F9]">
               {menuss.map((data, index) => {
                 return (
                   <li key={index}>
                     <a
                       onClick={() => setIsMenuOpen(!isMenuOpen)}
-                      className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      className="block px-3 py-2 text-black rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:p-0"
                       href={`#${data}`}
                     >
                       {data}
@@ -84,16 +82,16 @@ function Navbar() {
 
           {/* Desktop Navbar */}
           <div className="hidden md:flex md:w-auto" id="navbar-default-desktop">
-            <ul className="flex flex-col p-4 mt-4 font-medium ease-in-out border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-[#F7F9F9] dark:border-gray-700">
+            <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-[#F7F9F9]">
               {menuss.map((data, index) => {
                 return (
                   <li key={index}>
                     <a
-                      className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                      href={`#${data}`}
-                    >
-                      {data}
-                    </a>
+  className="block px-3 py-2 text-black rounded hover:bg-gray-200 hover:text-blue-700 md:hover:bg-transparent md:border-0 md:p-0"
+  href={`#${data}`}
+>
+  {data}
+</a>
                   </li>
                 );
               })}
